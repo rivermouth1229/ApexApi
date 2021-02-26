12e329d9425cae28a653634c5226d2a9af8531a3
kobayashitakaki <o.share.thief@gmail.com>
2021-02-27 00:41:38 +0900

    ApexStatus 取得処理実装

--- app/models/ApexStatus.js
+++ app/models/ApexStatus.js
@@ -1,8 +1,29 @@
 "use strict";
 
+require("dotenv").config();
+const fetch = require("node-fetch");
+
 exports.ApexStatus = class ApexStatus {
-  static fetch(psnId) {
-    if (process.env.NODE_ENV !== "production") {
+  static async fetchStatus(psnId) {
+    if (process.env.ENV_APEX_API_KEY) {
+      const baseUrl =
+        "https://public-api.tracker.gg/v2/apex/standard/profile/psn/";
+      const apiKey = process.env.ENV_APEX_API_KEY;
+      let apiUrl = `${baseUrl}${psnId}?TRN-Api-Key=${apiKey}`;
+      const response = await fetch(apiUrl);
+      const { data } = await response.json();
+      let ret = {};
+      ret.psnId = data.platformInfo.platformUserId;
+      ret.rankValue = data.segments[0].stats.rankScore.value;
+      ret.rankIconPath = data.segments[0].stats.rankScore.metadata.iconUrl;
+      let activeLegend = data.metadata.activeLegendName;
+      let query = data.segments.filter((x) => x.metadata.name == activeLegend);
+      if (query.length === 1) {
+        ret.legendImagePath = query[0].metadata.imageUrl;
+        ret.backgroundPath = query[0].metadata.bgImageUrl;
+      }
+      return ret;
+    } else {
       // それっぽい値のjsonを返す
       return new Promise((resolve) => setTimeout(() => resolve(), 500)).then(
         () => {
@@ -18,10 +39,6 @@ exports.ApexStatus = class ApexStatus {
           };
         }
       );
-    } else {
-      // 未実装
-      // APIからデータを取得して、整形したjsonを返す
-      return {};
     }
   }
 };
